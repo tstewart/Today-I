f4d3f1b897b101cfaab5510076ad8b60346cd3db
T. Stewart <tomstewart20@gmail.com>
2020-11-03 12:02:50 +0000

    Added selectable date to calender view

--- app/src/main/java/io/github/tstewart/todayi/CalenderViewActivity.java
+++ app/src/main/java/io/github/tstewart/todayi/CalenderViewActivity.java
@@ -9,14 +9,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class CalenderViewActivity extends AppCompatActivity {
+
+    CalendarView datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender_view);
 
+        datePicker = findViewById(R.id.calendarView);
+        datePicker.setOnDateChangeListener(this::onCalenderClick);
+    }
+
+    private void onCalenderClick(CalendarView calendarView, int year, int month, int day) {
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
     }
 
 }
