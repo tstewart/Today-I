b68357ed2ac187e2e85c3438ca5cc236d4c548df
T. Stewart <tomstewart20@gmail.com>
2020-11-03 12:15:28 +0000

    Added options menu layout

--- app/src/main/java/io/github/tstewart/todayi/OptionsActivity.java
+++ app/src/main/java/io/github/tstewart/todayi/OptionsActivity.java
@@ -9,25 +9,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class OptionsActivity extends AppCompatActivity {
+
+    Button exportDataButton;
+    Button googleSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        exportDataButton = findViewById(R.id.buttonExportData);
+        googleSignInButton = findViewById(R.id.buttonGoogleSignIn);
+
+        exportDataButton.setOnClickListener(this::onExportDataButtonClicked);
+        googleSignInButton.setOnClickListener(this::onGoogleSignInButtonClicked);
     }
+
+    private void onExportDataButtonClicked(View view) {
+        //TODO
+    }
+
+    private void onGoogleSignInButtonClicked(View view) {
+        //TODO
+    }
+
 
 }
