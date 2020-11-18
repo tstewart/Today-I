f140777a445edaedb0ef95f4f803873a1a74823e
T. Stewart <tomstewart20@gmail.com>
2020-11-18 17:30:44 +0000

    Added ripple effect to day rating buttons

--- app/src/main/java/io/github/tstewart/todayi/ui/fragment/DayRatingFragment.java
+++ app/src/main/java/io/github/tstewart/todayi/ui/fragment/DayRatingFragment.java
@@ -1,32 +1,30 @@
 package io.github.tstewart.todayi.ui.fragment;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import io.github.tstewart.todayi.R;
 
 public class DayRatingFragment extends Fragment {
 
+    private GradientDrawable buttonDrawable;
     Button[] buttons;
     int colors[] = new int[]{R.color.colorRatingRed, R.color.colorRatingOrange, R.color.colorRatingYellow, R.color.colorRatingLightGreen, R.color.colorRatingGreen};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,11 +41,12 @@ public class DayRatingFragment extends Fragment {
 
         for (int i = 0; i < 5; i++) {
 
-            buttons[i] = new Button(new ContextThemeWrapper(getContext(), R.style.Widget_AppCompat_Button_Borderless), null, R.style.Widget_AppCompat_Button_Borderless);
+            buttons[i] = new Button(new ContextThemeWrapper(getContext(), R.style.AppTheme_DayRatingButton), null, R.style.Widget_AppCompat_Button_Borderless);
             buttons[i].setText(String.valueOf(i+1));
             buttons[i].setOnClickListener(this::onRatingButtonClicked);
 
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+
             ll.addView(buttons[i], layoutParams);
         }
     }
@@ -67,13 +66,20 @@ public class DayRatingFragment extends Fragment {
 
                 if (index >= 0 && index < colors.length) {
                     int color = colors[index];
-                    buttonClicked.setBackgroundColor(ContextCompat.getColor(context, color));
+                    setButtonBackground(buttonClicked, color);
                 }
             }
         }
     }
 
     private void resetButtonBackground(Button button) {
-        if(button != null) button.setBackgroundColor(0x00000000);
+        setButtonBackground(button, R.color.colorTransparent);
+    }
+
+    private void setButtonBackground(Button button, int color) {
+        if(getContext() != null && button != null) {
+            GradientDrawable drawable = (GradientDrawable) button.getBackground();
+            drawable.setColor(ContextCompat.getColor(getContext(), color));
+        }
     }
 }
