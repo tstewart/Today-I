98ee53e799e44842b5727bdc758c7ab55d1e03d0
T. Stewart <tomstewart20@gmail.com>
2020-11-23 16:52:28 +0000

    Implemented day rating and day post decorations to calendar view

--- app/src/main/java/io/github/tstewart/todayi/CalendarViewActivity.java
+++ app/src/main/java/io/github/tstewart/todayi/CalendarViewActivity.java
@@ -4,17 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
-import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.ZoneId;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -30,6 +28,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import io.github.tstewart.todayi.sql.DBConstants;
 import io.github.tstewart.todayi.sql.Database;
 import io.github.tstewart.todayi.sql.DatabaseHelper;
+import io.github.tstewart.todayi.ui.decorator.DayPostedDecorator;
+import io.github.tstewart.todayi.ui.decorator.DayRatedDecorator;
+import io.github.tstewart.todayi.ui.decorator.DayRatingSplitter;
+
 
 public class CalendarViewActivity extends AppCompatActivity {
 
@@ -37,8 +39,8 @@ public class CalendarViewActivity extends AppCompatActivity {
 
     Date selectedDate = new Date();
 
-    List<Date> daysPostedOn;
-    HashMap<Date, Integer> ratings;
+    List<CalendarDay> daysPostedOn;
+    HashMap<CalendarDay, Integer> ratings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,16 +68,22 @@ public class CalendarViewActivity extends AppCompatActivity {
 
         daysPostedOn = getPostedDates();
         ratings = getDaysRated();
+
+        DayPostedDecorator daysPostedDecorator = new DayPostedDecorator(daysPostedOn);
+        List<DayRatedDecorator> dayRatedDecorators = new DayRatingSplitter(this).getDayRatingDecorators(ratings);
+
+        calendarView.addDecorator(daysPostedDecorator);
+        calendarView.addDecorators(dayRatedDecorators);
     }
 
     // Return a list of events matching dates that accomplishments have been posted on
 
-    private List<Date> getPostedDates() {
+    private List<CalendarDay> getPostedDates() {
 
         Database db = new Database(this);
         SQLiteDatabase sqlDb = db.getReadableDatabase();
 
-        List<Date> dates = new ArrayList<>();
+        List<CalendarDay> dates = new ArrayList<>();
 
         Cursor cursor = sqlDb.rawQuery(DBConstants.ACCOMPLISHMENT_DATE_GROUP_QUERY, null);
 
@@ -88,7 +96,9 @@ public class CalendarViewActivity extends AppCompatActivity {
                         Date date = new SimpleDateFormat(DBConstants.DATE_FORMAT, Locale.getDefault()).parse(dateString);
 
                         if(date != null) {
-                            dates.add(date);
+                            LocalDate localDate = Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+                            dates.add(CalendarDay.from(localDate));
                         }
 
                     } catch (ParseException e) {
@@ -103,10 +113,10 @@ public class CalendarViewActivity extends AppCompatActivity {
         return dates;
     }
 
-    private HashMap<Date, Integer> getDaysRated() {
+    private HashMap<CalendarDay, Integer> getDaysRated() {
         SQLiteDatabase db = new DatabaseHelper(DBConstants.RATING_TABLE).getDatabase(getApplicationContext());
 
-        HashMap<Date, Integer> ratings = new HashMap<>();
+        HashMap<CalendarDay, Integer> ratings = new HashMap<>();
 
         Cursor cursor = db.rawQuery(DBConstants.DAY_RATING_ALL_RESULTS_QUERY, null);
 
@@ -118,7 +128,9 @@ public class CalendarViewActivity extends AppCompatActivity {
                 try {
                     Date date = new SimpleDateFormat(DBConstants.DATE_FORMAT, Locale.getDefault()).parse(dateString);
                     if(date != null) {
-                        ratings.put(date,rating);
+                        LocalDate localDate = Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+                        ratings.put(CalendarDay.from(localDate),rating);
                     }
 
                 } catch (ParseException e) {
