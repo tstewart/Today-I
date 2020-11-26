7830d037859e4bd761e03d7cada42351969fc0e2
tom <tomstewart20@gmail.com>
2020-11-26 11:32:44 +0000

    Added populate accomplishments debug feature

--- app/src/main/java/io/github/tstewart/todayi/DebugActivity.java
+++ app/src/main/java/io/github/tstewart/todayi/DebugActivity.java
@@ -7,6 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+import io.github.tstewart.todayi.object.Accomplishment;
+import io.github.tstewart.todayi.sql.DBConstants;
+import io.github.tstewart.todayi.sql.DatabaseHelper;
+
 public class DebugActivity extends AppCompatActivity {
 
     @Override
@@ -17,13 +26,16 @@ public class DebugActivity extends AppCompatActivity {
         this.setTitle("Debug Menu <3");
 
         Button invalidateBackupButton = findViewById(R.id.debugInvalidateBackupTime);
+        Button populateAccomplishmentsButton = findViewById(R.id.debug_populate_accomplishments);
         Button backButton = findViewById(R.id.debugBack);
 
         if(invalidateBackupButton != null) invalidateBackupButton.setOnClickListener(this::onInvalidateBackupButtonClicked);
+        if(populateAccomplishmentsButton != null) populateAccomplishmentsButton.setOnClickListener(this::onPopulateAccomplishmentsButtonClicked);
         if(backButton != null) backButton.setOnClickListener(view -> {
             this.finish();
         });
     }
+
 
     private void onInvalidateBackupButtonClicked(View view) {
         try {
@@ -37,5 +49,39 @@ public class DebugActivity extends AppCompatActivity {
         catch(NullPointerException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void onPopulateAccomplishmentsButtonClicked(View view) {
+        Random random = new Random();
+        Date targetDate = new Date();
+        Date currentDate = getDateMonthAgo();
+
+        DatabaseHelper helper = new DatabaseHelper(DBConstants.ACCOMPLISHMENT_TABLE);
+
+        while(currentDate.before(targetDate)) {
+            int numPosts = random.nextInt(5);
+
+            for (int i = 0; i < numPosts; i++) {
+                Accomplishment accomplishment = new Accomplishment(currentDate, "DUMMY CONTENT!");
+                helper.insert(this, accomplishment);
+            }
+
+            currentDate = addDay(currentDate);
+        }
+    }
+
+    private Date addDay(Date currentDate) {
+        Calendar c = new GregorianCalendar();
+        c.setTime(currentDate);
+        c.add(Calendar.DAY_OF_YEAR, 1);
+        return c.getTime();
+    }
+
+    private Date getDateMonthAgo() {
+        Calendar c = new GregorianCalendar();
+        c.setTime(new Date());
+        c.add(Calendar.DAY_OF_YEAR, -31);
+
+        return c.getTime();
     }
 }
