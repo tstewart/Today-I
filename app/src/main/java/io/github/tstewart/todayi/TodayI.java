1e0991b0de7600c3cdb2748041697e5cdd5cf149
T. Stewart <tomstewart20@gmail.com>
2020-12-04 17:03:23 +0000

    Added functions to get user preferences from SharedPreferences

--- app/src/main/java/io/github/tstewart/todayi/TodayI.java
+++ app/src/main/java/io/github/tstewart/todayi/TodayI.java
@@ -11,6 +11,7 @@ import java.util.GregorianCalendar;
 
 import androidx.annotation.NonNull;
 import io.github.tstewart.todayi.data.LocalDatabaseIO;
+import io.github.tstewart.todayi.data.UserPreferences;
 import io.github.tstewart.todayi.errors.ExportFailedException;
 import io.github.tstewart.todayi.data.DBConstants;
 import io.github.tstewart.todayi.helpers.DatabaseHelper;
@@ -29,19 +30,29 @@ public class TodayI extends Application {
     /* Backup Database every x hours */
     private static final int BACKUP_EVERY_HOURS = 24;
 
+    /* User preferences */
+    private UserPreferences mPreferences;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        SharedPreferences sharedPrefs = getSharedPreferences(getString(R.string.user_prefs_file_location_key), MODE_PRIVATE);
+        mPreferences = new UserPreferences(sharedPrefs);
+
+        /* Set default values for user preferences if they do not exist */
+        mPreferences.setDefaultValue(getString(R.string.user_prefs_gestures_enabled), true);
+        mPreferences.setDefaultValue(getString(R.string.user_prefs_clip_empty_lines), true);
+
         /* Database auto backup management */
         Context context = getApplicationContext();
-        boolean shouldBackup = false;
 
-        if (context != null) {
-            SharedPreferences sharedPrefs = context.getSharedPreferences(getString(R.string.user_prefs_file_location_key), MODE_PRIVATE);
 
-            if (sharedPrefs.contains(getString(R.string.user_prefs_last_backed_up_key))) {
-                long lastBackedUp = sharedPrefs.getLong(getString(R.string.user_prefs_last_backed_up_key), -1);
+        if (context != null && sharedPrefs != null) {
+            boolean shouldBackup = false;
+
+            if (mPreferences.contains(getString(R.string.user_prefs_last_backed_up_key))) {
+                long lastBackedUp = (Long)mPreferences.get(getString(R.string.user_prefs_last_backed_up_key), -1);
                 /* If the default value is selected, the app should attempt to backup data.
                 If the last backup time is in the future (due to time modifications then we should backup anyway. (This may be changed)
                 Additionally, If the app hasn't backed up within the number of hours defined in BACKUP_EVERY_HOURS, the app should attempt to backup data */
@@ -61,9 +72,7 @@ public class TodayI extends Application {
                     LocalDatabaseIO.backupDb(this, DBConstants.DB_NAME);
 
                     /* Set last time backed up */
-                    sharedPrefs.edit()
-                            .putLong(getString(R.string.user_prefs_last_backed_up_key), System.currentTimeMillis())
-                            .apply();
+                    mPreferences.set(getString(R.string.user_prefs_last_backed_up_key),System.currentTimeMillis());
                     Log.i(CLASS_LOG_TAG, "Application data backed up!");
 
                 } catch (ExportFailedException e) {
--- app/src/main/java/io/github/tstewart/todayi/data/UserPreferences.java
+++ app/src/main/java/io/github/tstewart/todayi/data/UserPreferences.java
@@ -1,5 +1,7 @@
 package io.github.tstewart.todayi.data;
 
+import android.content.SharedPreferences;
+
 /*
 User preferences constants.
 TODO For now, this does not link with Android preferences or Settings
@@ -12,7 +14,51 @@ public class UserPreferences {
     /* Whether or not to remove empty lines from Accomplishment content */
     private static final boolean ACCOMPLISHMENT_REMOVE_EMPTY_LINES = true;
 
-    private UserPreferences(){}
+    /* Preferences location to check */
+    private final SharedPreferences mPreferences;
+
+    public UserPreferences(SharedPreferences prefs) {
+        this.mPreferences = prefs;
+    }
+
+    /* Get object from preferences, or default value if not found */
+    public Object get(String key, Object defaultValue) {
+        if(mPreferences != null) {
+            Object value = mPreferences.getAll().get(key);
+            /* Return default value if not found, otherwise return found value */
+            return (value == null) ? defaultValue : value;
+        }
+        return defaultValue;
+    }
+
+    /* Save value to user preferences.
+    * If value couldn't be saved, return false */
+    public boolean set(String key, Object value) {
+        /* Shared Preferences will only allow saving of these specific types */
+
+        SharedPreferences.Editor editor = mPreferences.edit();
+
+        if(value instanceof String) editor.putString(key, (String) value);
+        else if(value instanceof Boolean) editor.putBoolean(key, (Boolean) value);
+        else if(value instanceof Long) editor.putLong(key, (Long) value);
+        else if(value instanceof Float) editor.putFloat(key, (Float) value);
+        else return false;
+
+        editor.apply();
+        return true;
+    }
+
+    /* Set default preference value, if the value doesn't already exist in preferences */
+    public boolean setDefaultValue(String key, Object value) {
+        if(!mPreferences.contains(key)) {
+            return set(key,value);
+        }
+        return false;
+    }
+
+    public boolean contains(String key) {
+        return mPreferences.contains(key);
+    }
 
     public static int getMaxDayRating() {
         return MAX_DAY_RATING;
